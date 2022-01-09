4f72cd2b28a2e005053b0c5c8411c0043698655d
Liang <taylor721@gmail.com>
2022-01-09 15:55:15 -0700

    logo design 2022-1-9

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
+import "./header.style.scss";
+
+import logo from "../../asset/logo_s.png";
+import Nav from "../nav/nav.component";
 
 const Header = ({ test }) => (
   <div className="header bg-light-gray">
     <div className="header-container bg-col1 container-xxl">
-      <div className="row">
-        <div className="logo-container bg-col2 col-md-4">logo</div>
-        <div className="nav-container bg-col3 col-md-8">nav</div>
+      <div className="bg-col3 logo-container row  p-2">
+       <div className="logo-img bg-white col-4 d-flex justify-content-center align-items-center ">
+			 	<img src={ logo } className="logo" alt="" />
+			 </div>
+			 <div className="logo-text col-8 d-flex justify-content-center align-items-center">
+			 	<span className="oaple text-uppercase">oaple</span>
+				<span className="design text-uppercase">design</span>
+			 </div>
       </div>
+      <Nav className="nav-container col-sm-8" />
     </div>
   </div>
 );
---
+++ src/component/nav/nav.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './nav.style.scss'
+
+const Nav = () => (
+    <div className="nav-container bg-color2">Navaaaa</div>
+)
+
+export default Nav;
