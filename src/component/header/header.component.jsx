d6ad9af101ea793adb020b3cb6ea726d7b0ee73f
Liang <taylor721@gmail.com>
2022-01-09 10:33:37 -0700

    layout of header

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-const Header = ({test}) => (
-   <div className="header text-primary">
-   Public header
-   {test}
-   </div>
-)
+const Header = ({ test }) => (
+  <div className="header bg-light-gray">
+    <div className="header-container bg-col1 container-xxl">
+      <div className="row">
+        <div className="logo-container bg-col2 col-md-4">logo</div>
+        <div className="nav-container bg-col3 col-md-8">nav</div>
+      </div>
+    </div>
+  </div>
+);
 
-const mapStateToProps = state => ({
-   test: state.header.test
-})
-
+const mapStateToProps = (state) => ({
+  test: state.header.test,
+});
 
 export default connect(mapStateToProps)(Header);
