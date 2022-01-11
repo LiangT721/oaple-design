a8e6a8a6c517be1c3b9fc95fde9a8b6cc8d645f2
Liang <taylor721@gmail.com>
2022-01-11 15:10:26 -0700

    add landingpage slider

--- src/pages/landingpage/landingpage.component.jsx
+++ src/pages/landingpage/landingpage.component.jsx
@@ -1,16 +1,34 @@
 import React, { Fragment } from "react";
 
-import './landingpage.style.scss';
+import testPic1 from '../../asset/testPic1.jpg'
+import testPic2 from '../../asset/testPic2.jpg'
+import Carousel from "react-bootstrap/Carousel";
+
+import "./landingpage.style.scss";
 
 const Landingpage = () => {
-    return(
-        <Fragment>
-            <div className="landingpage bg-color1">
-                Landingpage content
-                <div className="test"></div>
-            </div>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <div className="landingpage bg-color1">
+        <Carousel className="h-100">
+          <Carousel.Item className='h-100' interval={1500}>
+            <img
+              className="slider-img d-block w-100"
+              src={testPic1}
+              alt="Image One"
+            />
+          </Carousel.Item>
+          <Carousel.Item className='h-100' interval={500}>
+            <img
+              className="slider-img d-block w-100"
+              src={testPic2}
+              alt="Image Two"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </Fragment>
+  );
+};
 
 export default Landingpage;
